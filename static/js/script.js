db844ebd1776795c3b9842cc16e03b4c53d8095b
Monika Hrda <50584912+monika-hrda@users.noreply.github.com>
2021-09-11 20:06:48 +0100

    feat wip: Add basic login form and add functionality in script.js to show password (#81)

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,14 @@
   // -------------------------------------------------------------------- Copyright
 
+function showPassword() {
+  var input = document.getElementById("password");
+  if (input.type === "password") {
+    input.type = "text";
+  } else {
+    input.type = "password";
+  }
+}
+
 function copyrightYear() {
     var d = new Date();
     var y = d.getFullYear();

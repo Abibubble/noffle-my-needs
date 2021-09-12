416fc77a58fad03219a830a312bbaa35455b0c79
Abibubble <abi.yamaha.neo@gmail.com>
2021-09-12 15:01:19 +0000

    feat: Add favicon

--- app.py
+++ app.py
@@ -350,4 +350,4 @@ def delete_account(username):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=False)
+            debug=True)

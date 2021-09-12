191ff43e05c611702ae074c34efdb56a7048408a
Abibubble <abi.yamaha.neo@gmail.com>
2021-09-12 02:03:06 +0000

    maint: debug to false

--- app.py
+++ app.py
@@ -277,4 +277,4 @@ def delete_account(username):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
